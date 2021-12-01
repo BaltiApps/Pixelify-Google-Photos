a0ff72b9d114833448cdc7675d78c08d3d7464b9
SayantanRC <sayantan_rc@protonmail.com>
2021-12-02 03:25:44 +0530

    Create blank main activity

---
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/ActivityMain.kt
@@ -0,0 +1,14 @@
+package balti.xposed.pixelifygooglephotos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityMain: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
