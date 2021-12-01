e9ec587b44d504d8632d49a5510400d0baefd70c
SayantanRC <sayantan_rc@protonmail.com>
2021-12-02 03:52:28 +0530

    Show snackbar for reboot

--- app/src/main/java/balti/xposed/pixelifygooglephotos/ActivityMain.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/ActivityMain.kt
@@ -1,16 +1,23 @@
 package balti.xposed.pixelifygooglephotos
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import balti.xposed.pixelifygooglephotos.Constants.PREF_STRICTLY_CHECK_GOOGLE_PHOTOS
 import balti.xposed.pixelifygooglephotos.Constants.PREF_USE_PIXEL_2016
 import balti.xposed.pixelifygooglephotos.Constants.SHARED_PREF_FILE_NAME
+import com.google.android.material.snackbar.Snackbar
 
 class ActivityMain: AppCompatActivity() {
 
     private val pref by lazy {
         getSharedPreferences(SHARED_PREF_FILE_NAME, MODE_PRIVATE)
+    }
+
+    private fun showRebootSnack(){
+        val rootView = findViewById<LinearLayout>(R.id.root_view_for_snackbar)
+        Snackbar.make(rootView, R.string.please_reboot, Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,7 @@ class ActivityMain: AppCompatActivity() {
             pref.edit().apply {
                 putBoolean(PREF_USE_PIXEL_2016, isChecked)
                 apply()
+                showRebootSnack()
             }
         }
 
@@ -32,6 +40,7 @@ class ActivityMain: AppCompatActivity() {
             pref.edit().apply {
                 putBoolean(PREF_STRICTLY_CHECK_GOOGLE_PHOTOS, isChecked)
                 apply()
+                showRebootSnack()
             }
         }
     }
