728c0a4dc519fd4f9291c583e4baed51d6faf4d6
SayantanRC <sayantan_rc@protonmail.com>
2021-12-08 04:58:50 +0530

    ActivityMain.kt: launch FeatureCustomize

--- app/src/main/java/balti/xposed/pixelifygooglephotos/ActivityMain.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/ActivityMain.kt
@@ -1,8 +1,11 @@
 package balti.xposed.pixelifygooglephotos
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import balti.xposed.pixelifygooglephotos.Constants.PREF_DEVICE_TO_SPOOF
@@ -37,6 +40,18 @@ class ActivityMain: AppCompatActivity(R.layout.activity_main) {
     }
 
     private val utils by lazy { Utils() }
+
+    /**
+     * Activity launcher for [FeatureCustomize] activity.
+     * If user presses "Save" on [FeatureCustomize] activity, then result code is RESULT_OK.
+     * Then show prompt to force stop Google Photos.
+     */
+    private val childActivityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                showRebootSnack()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -131,6 +146,13 @@ class ActivityMain: AppCompatActivity(R.layout.activity_main) {
          */
         openGooglePhotos.setOnClickListener {
             utils.openApplication(Constants.PACKAGE_NAME_GOOGLE_PHOTOS, this)
+        }
+
+        /**
+         * Launch [FeatureCustomize] to fine select the features.
+         */
+        customizeFeatureFlags.setOnClickListener {
+            childActivityLauncher.launch(Intent(this, FeatureCustomize::class.java))
         }
 
         utils.fixPermissions(packageName)
