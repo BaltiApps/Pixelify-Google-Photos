5a71a945d3f150857b49d98d707f63517a34c4fe
SayantanRC <sayantan_rc@protonmail.com>
2021-12-08 07:04:11 +0530

    Constants.kt: Add link to repository for updates.

--- app/src/main/java/balti/xposed/pixelifygooglephotos/Constants.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/Constants.kt
@@ -3,7 +3,15 @@ package balti.xposed.pixelifygooglephotos
 object Constants {
 
     val PACKAGE_NAME_GOOGLE_PHOTOS = "com.google.android.apps.photos"
+
     val TELEGRAM_GROUP = "https://t.me/pixelifyGooglePhotos"
+
+    val UPDATE_INFO_URL = "https://raw.githubusercontent.com/BaltiApps/Pixelify-Google-Photos/main/update_info.json"
+    val UPDATE_INFO_URL2 = "https://raw.githubusercontent.com/Xposed-Modules-Repo/balti.xposed.pixelifygooglephotos/main/update_info.json"
+    val RELEASES_URL = "https://github.com/BaltiApps/Pixelify-Google-Photos/releases"
+    val RELEASES_URL2 = "https://github.com/Xposed-Modules-Repo/balti.xposed.pixelifygooglephotos/releases"
+
+    val FIELD_LATEST_VERSION_CODE = "latest_version_code"
 
     val SHARED_PREF_FILE_NAME = "prefs"
 
