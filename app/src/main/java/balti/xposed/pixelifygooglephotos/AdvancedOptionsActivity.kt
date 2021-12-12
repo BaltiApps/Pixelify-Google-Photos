8259adf3562627b3c9c206f8aafb8ab0f2531be1
SayantanRC <sayantan_rc@protonmail.com>
2021-12-12 10:49:42 +0530

    AdvancedOptionsActivity.kt: Use save button to save all preferences in one go. No need for editor in multiple places.

--- app/src/main/java/balti/xposed/pixelifygooglephotos/AdvancedOptionsActivity.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/AdvancedOptionsActivity.kt
@@ -2,6 +2,7 @@ package balti.xposed.pixelifygooglephotos
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import balti.xposed.pixelifygooglephotos.Constants.PREF_ENABLE_VERBOSE_LOGS
@@ -13,41 +14,33 @@ class AdvancedOptionsActivity: AppCompatActivity(R.layout.advanced_options_activ
         getSharedPreferences(SHARED_PREF_FILE_NAME, MODE_WORLD_READABLE)
     }
 
-    /**
-     * This will send [Activity.RESULT_OK] to caller activity.
-     * To be called when any option in this activity page is changed.
-     * Example in [CheckBox.setPreferenceState].
-     */
-    private fun setResultOk(){
-        setResult(Activity.RESULT_OK)
-    }
+    private val verboseLogging by lazy { findViewById<CheckBox>(R.id.verbose_logging) }
+    private val save by lazy { findViewById<Button>(R.id.save_advanced_option) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val verboseLogging = findViewById<CheckBox>(R.id.verbose_logging)
+        if (pref == null) return
 
-        verboseLogging.setPreferenceState(PREF_ENABLE_VERBOSE_LOGS, false)
+        verboseLogging.isChecked = pref.getBoolean(PREF_ENABLE_VERBOSE_LOGS, false)
+
+        save.setOnClickListener {
+            savePreferences()
+        }
+
     }
 
     /**
-     * Extension function on [CheckBox].
-     * Set initial value from SharedPreference.
-     * Modifies value in SharedPreference when any change in Checkbox state.
-     *
-     * @param preferenceKey Get value from this key in SharedPreference.
-     * @param defaultValue Value to set in Checkbox if no value is present
-     * for key [preferenceKey] in SharedPreference.
+     * Function to save all options.
      */
-    fun CheckBox.setPreferenceState(preferenceKey: String, defaultValue: Boolean){
-        isChecked = pref.getBoolean(preferenceKey, defaultValue)
-        setOnCheckedChangeListener { _, isChecked ->
-            setResultOk()
-            pref.edit().run {
-                putBoolean(preferenceKey, isChecked)
-                apply()
-            }
+    private fun savePreferences(){
+        pref?.edit()?.run {
+
+            putBoolean(PREF_ENABLE_VERBOSE_LOGS, verboseLogging.isChecked)
+
+            apply()
         }
+        setResult(Activity.RESULT_OK)
     }
 
 }
