393e336fe86f9599788c2bcc2e1245b52f730489
SayantanRC <sayantan_rc@protonmail.com>
2021-12-12 11:32:54 +0530

    AdvancedOptionsActivity.kt: set initial states of android version spoof.

--- app/src/main/java/balti/xposed/pixelifygooglephotos/AdvancedOptionsActivity.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/AdvancedOptionsActivity.kt
@@ -2,10 +2,13 @@ package balti.xposed.pixelifygooglephotos
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import balti.xposed.pixelifygooglephotos.Constants.PREF_DEVICE_TO_SPOOF
 import balti.xposed.pixelifygooglephotos.Constants.PREF_ENABLE_VERBOSE_LOGS
+import balti.xposed.pixelifygooglephotos.Constants.PREF_SPOOF_ANDROID_VERSION_FOLLOW_DEVICE
+import balti.xposed.pixelifygooglephotos.Constants.PREF_SPOOF_ANDROID_VERSION_MANUAL
 import balti.xposed.pixelifygooglephotos.Constants.SHARED_PREF_FILE_NAME
 
 class AdvancedOptionsActivity: AppCompatActivity(R.layout.advanced_options_activity) {
@@ -15,6 +18,10 @@ class AdvancedOptionsActivity: AppCompatActivity(R.layout.advanced_options_activ
     }
 
     private val verboseLogging by lazy { findViewById<CheckBox>(R.id.verbose_logging) }
+    private val deviceNameLabel by lazy { findViewById<TextView>(R.id.device_name_label) }
+    private val androidVersionRadioGroup by lazy { findViewById<RadioGroup>(R.id.android_version_radio_group) }
+    private val deviceAndroidVersion by lazy { findViewById<TextView>(R.id.device_android_version) }
+    private val androidVersionSpinner by lazy { findViewById<Spinner>(R.id.android_version_spinner) }
     private val save by lazy { findViewById<Button>(R.id.save_advanced_option) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +30,50 @@ class AdvancedOptionsActivity: AppCompatActivity(R.layout.advanced_options_activ
         if (pref == null) return
 
         verboseLogging.isChecked = pref.getBoolean(PREF_ENABLE_VERBOSE_LOGS, false)
+
+        /**
+         * Get the current spoofing device an its android version.
+         */
+        val deviceNameInPreference = pref.getString(PREF_DEVICE_TO_SPOOF, DeviceProps.defaultDeviceName)
+        val spoofDevice = DeviceProps.getDeviceProps(deviceNameInPreference)
+        deviceNameLabel.text = spoofDevice?.deviceName
+        deviceAndroidVersion.text = spoofDevice?.androidVersion?.label
+
+        /**
+         * Set spinner for manually setting android version.
+         */
+        androidVersionSpinner.apply {
+            val allVersionLabels = DeviceProps.allAndroidVersions.map { it.label }
+            val aa = ArrayAdapter(
+                this@AdvancedOptionsActivity,
+                android.R.layout.simple_spinner_item,
+                allVersionLabels
+            )
+
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            adapter = aa
+            val defaultSelection = pref?.getString(PREF_SPOOF_ANDROID_VERSION_MANUAL, null)
+            setSelection(aa.getPosition(defaultSelection))
+        }
+
+        /**
+         * Set default radio button.
+         */
+        androidVersionRadioGroup.apply {
+
+            // hide spinner if not to be manually set.
+            setOnCheckedChangeListener { _, checkedId ->
+                androidVersionSpinner.isVisible = checkedId == R.id.manually_set_android_version
+            }
+
+            check(
+                when {
+                    pref.getBoolean(PREF_SPOOF_ANDROID_VERSION_FOLLOW_DEVICE, false) -> R.id.follow_spoof_device_version
+                    pref.getString(PREF_SPOOF_ANDROID_VERSION_MANUAL, null) != null -> R.id.manually_set_android_version
+                    else -> R.id.dont_spoof_android_version
+                }
+            )
+        }
 
         save.setOnClickListener {
             savePreferences()
